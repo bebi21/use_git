a33c74ae79e4ac48af806574f20820200b7012be
bebi21 <nguyenlocnd18@gmail.com>
2024-03-27 14:47:47 +0700

    ad dang ki

--- src/login/Register.jsx
+++ src/login/Register.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <div>Register </div>
+      <span> Đây là đăng kí</span>
+    </>
+  );
 }

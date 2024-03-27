45fce22b88f0c1356491b625be3f2c597482eb70
Vantrung86 <buivantrungthanhlac@gmail.com>
2024-03-27 14:45:58 +0700

    add

--- src/login/Register.jsx
+++ src/login/Register.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      Register
+      <h1>Danh sach cong viec</h1>
+      <input type="text" />
+      <button>Add</button>
+    </div>
   )
 }
